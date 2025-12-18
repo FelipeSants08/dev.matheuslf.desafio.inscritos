bb17f26c3f2ea37a244e8b9f9847e1ae1bcea658
FelipeSants08 <felipe_santana.01@hotmail.com>
2025-12-18 12:26:55 -0300

    Atualizando a documentação do Swagger para receber um Token de JWT

--- src/main/java/dev/matheuslf/desafio/inscritos/config/OpenApiConfig.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package dev.matheuslf.desafio.inscritos.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +15,15 @@ public class OpenApiConfig {
     public OpenAPI customOpenApi(){
 
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
                 .info(new Info()
                         .title("Desafio Matheus")
                         .version("1.0")
